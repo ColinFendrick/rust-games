49f8006e073f362bbcedca5ec9f7de8ee71bb0e2
Colin Fendrick <colin.fendrick@gmail.com>
2020-12-22 08:36:54 -0800

    Guessing game

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,3 +1,21 @@
+use rand::{thread_rng, Rng};
+use std::io::{stdin, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+	let mut rng = thread_rng();
+	let random = rng.gen_range(1..100 + 1);
+	println!("Guess a number between 1 and 100");
+	for line in stdin().lock().lines() {
+	let parsed = line.ok().as_deref().map(str::parse::<i64>);
+	if let Some(Ok(guess)) = parsed {
+		match guess {
+				_ if guess < random => println!("Too low"),
+				_ if guess > random => println!("Too high"),
+				_ => {
+					println!("That's right");
+					break;
+				}
+			}
+		}
+	}
 }
